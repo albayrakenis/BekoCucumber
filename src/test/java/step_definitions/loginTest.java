9eccfc5a711979abda36d893d564b2b62be03a5d
Enis <albayrakenis>
2022-08-24 09:21:35 +0300

    bekoLoginTestCucumber

---
+++ src/test/java/step_definitions/loginTest.java
@@ -0,0 +1,27 @@
+package step_definitions;
+
+import io.cucumber.java.en.*;
+
+public class loginTest {
+
+    @Given("user go to the login page")
+    public void user_go_to_the_login_page() {
+        
+    }
+    @When("user enters correct credentials")
+    public void user_enters_correct_credentials() {
+        
+    }
+    @Then("user should be in {string}")
+    public void user_should_be_in(String string) {
+        
+    }
+
+    @When("user enters wrong credentials")
+    public void userEntersWrongCredentials() {
+    }
+
+    @Then("user should be at {string}")
+    public void userShouldBeAt(String string2) {
+    }
+}
