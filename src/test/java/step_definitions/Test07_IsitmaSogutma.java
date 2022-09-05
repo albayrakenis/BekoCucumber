5a603453c935d8d7bb03136d8f524d40fab66597
Enis <albayrakenis>
2022-09-05 10:11:52 +0300

    bekoLoginTestCucumber

---
+++ src/test/java/step_definitions/Test07_IsitmaSogutma.java
@@ -0,0 +1,16 @@
+package step_definitions;
+
+import io.cucumber.java.en.When;
+import pages.BeyazEsya;
+
+public class Test07_IsitmaSogutma {
+
+    BeyazEsya byzEsya =new BeyazEsya();
+
+    @When("Kullanici Isitma Sogutma tiklamali")
+    public void kullanici_isitma_sogutma_tiklamali() throws InterruptedException {
+
+        byzEsya.plpdeUrunAdiveKategoriEslestirme(byzEsya.IsitmaSogutma,byzEsya.klima);
+    }
+
+}
---
+++ target/test-classes/features/isitmaSogutma.feature
@@ -0,0 +1,8 @@
+Feature: Isıtma Sogutma Listesindeki Urunleri Kontrol
+
+  @wip
+  Scenario: Isıtma Sogutma Kategorisi Ürünleri Kontrol Etme
+    Given Kullanici giris sayfasina gitmeli
+    When Kullanici urunlere tiklamali
+    And Kullanici Isitma Sogutma tiklamali
+    Then Plp urunler ve kategori adi ayni olmali
